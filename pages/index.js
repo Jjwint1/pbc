873eb38608fa5c677db0ec91ee8a23ca68759fcd
Jjwint1 <jjw79@cam.ac.uk>
2021-02-05 01:14:43 +0000

    initial commit - landing page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import { motion } from 'framer-motion'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <>
+      <Head><title>Peterhouse Boat Club</title></Head>
+      <div className={styles.gradient}>
+        <motion.div className={styles.imgContainer}>
+          <motion.img src="BH.png" 
+            initial={{
+              opacity: 0,
+              bottom: '-50px',
+            }}
+            animate={{
+              opacity: 1,
+              bottom: 0,
+              transition: {
+                duration: 2,
+              }
+            }}
+          />
+        </motion.div>
+      </div>
+      <motion.div className={styles.header}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          transition: {
+            duration: 2,
+          }
+        }}
+      >
+        <p className={styles.title}>PETERHOUSE BOAT CLUB</p>
+        <p className={styles.welcome}>Welcome! Here you'll find updates about PBC, and info on how to join the club. Members can also book a boat or an erg.</p>
+      </motion.div>
+    </>
   )
 }
